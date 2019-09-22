23e83ebe51e2c3128234352f18201503b1ba55a8
Cameron Cline <camcline@iu.edu>
2019-09-22 19:03:39 -0400

    App Folder Creation

---
+++ app/public/js/user.app.js
@@ -0,0 +1,23 @@
+var waitingApp = new Vue({
+el: '#user',
+data: {
+  user: {
+    name:"",
+    dob:"",
+    age:"",
+    email:"",
+    nat:"",
+    picture:""
+  }
+},
+methods: {
+  fetchUser() {
+    fetch('https://randomuser.me/api')
+    .then(response => response.json())
+    .then(json => {waitingApp.user = json.results[0]});
+  }
+},
+created() {
+  this.fetchUser()
+}
+});
